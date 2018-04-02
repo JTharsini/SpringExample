0ee0dd159073939495deba55323756b7f3a4d8db
Jeyaganeshan Jeyatharsini <JJeyatharsini@cambio.se>
2018-04-02 19:45:00 +0530

    Configure attribute with value

--- src/main/java/com/jeya/spring/Triangle.java
+++ src/main/java/com/jeya/spring/Triangle.java
@@ -1,8 +1,18 @@
 package com.jeya.spring;
 
 public class Triangle {
+	private String type;
+	
 	public void draw()
 	{
-		System.out.println("Triangle drawn");
+		System.out.println(getType() + " triangle drawn");
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 }
