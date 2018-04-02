4c11011de65b41d582129244317d59f028aea0a7
Jeyaganeshan Jeyatharsini <JJeyatharsini@cambio.se>
2018-04-02 19:35:54 +0530

    By bean factory

--- src/main/java/com/jeya/spring/DrawingApp.java
+++ src/main/java/com/jeya/spring/DrawingApp.java
@@ -6,6 +6,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class DrawingApp {
 
 	public static void main(String[] args) {
+		/*		
+		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		Triangle triangle = (Triangle)factory.getBean("triangle");
+		triangle.draw();
+		
+		deprecated: spring.xml file should be placed in root directory of project
+		ApplicationContext is used to do the same job by BeanFactory and it provides
+		some additional features also
+		
+		BeanFactory is suitable only when resources are so crucial
+		*/
+		
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 		Triangle triangle = (Triangle) context.getBean("triangle");
 		triangle.draw();
