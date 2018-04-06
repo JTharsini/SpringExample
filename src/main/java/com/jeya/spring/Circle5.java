5755a6beb939164593ee726d077ced31990a8fc0
Jeyaganeshan Jeyatharsini <JJeyatharsini@cambio.se>
2018-04-06 16:49:50 +0530

    resource annotation without name

--- src/main/java/com/jeya/spring/Circle5.java
+++ src/main/java/com/jeya/spring/Circle5.java
@@ -4,10 +4,12 @@ import javax.annotation.Resource;
 
 public class Circle5 implements Shape {
 	private Point center;
+	private Radius radius;
 
 	@Override
 	public void draw() {
 		System.out.println("Center is : (" + center.getX() + "," + center.getY() + ")");
+		System.out.println("Radius is : " + radius.getValue());
 	}
 
 	public Point getCenter() {
@@ -17,5 +19,14 @@ public class Circle5 implements Shape {
 	@Resource(name="pointB")
 	public void setCenter(Point center) {
 		this.center = center;
+	}
+
+	public Radius getRadius() {
+		return radius;
+	}
+
+	@Resource
+	public void setRadius(Radius radius) {
+		this.radius = radius;
 	}
 }
---
+++ src/main/java/com/jeya/spring/Radius.java
@@ -0,0 +1,13 @@
+package com.jeya.spring;
+
+public class Radius {
+	private int value;
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+}
